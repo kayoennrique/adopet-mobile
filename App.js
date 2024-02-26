64e58a4b85976ac8ddbb1e8e892005fdce717529
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-25 23:11:03 -0300

    feat(git): shipping Container

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import 'react-native-gesture-handler';
 import React from 'react';
 import { useFonts, Poppins_400Regular } from '@expo-google-fonts/poppins';
-import Home from './src/pages/Home';
+import Navigation from './src/routes/navigation';
 export default function App() {
   let [fontsLoaded, fontError] = useFonts({
     "PoppinsRegular": Poppins_400Regular,
@@ -12,6 +12,6 @@ export default function App() {
   }
 
   return (
-    <Home />
+    <Navigation />
   );
 }
