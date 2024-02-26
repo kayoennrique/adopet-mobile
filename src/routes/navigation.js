eff62a33749e906e045590e93bd5b08b9888c219
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-25 23:19:05 -0300

    feat(git): creation of stack navigator

--- src/routes/navigation.js
+++ src/routes/navigation.js
@@ -1,9 +1,17 @@
 import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+
+const Stack = createStackNavigator();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
-
+      <Stack.Navigator>
+        <Stack.Screen name='Home' component={Home} />
+        <Stack.Screen name='Login' component={Login} />
+      </Stack.Navigator>
     </NavigationContainer>
   );
 }
