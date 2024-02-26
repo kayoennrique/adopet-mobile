dd9c30f0ccc796706809b7960c56928a19fef772
kayoennrique <138163354+kayoennrique@users.noreply.github.com>
2024-02-26 00:11:11 -0300

    build(npm): installing tab navigation

--- src/routes/navigation.js
+++ src/routes/navigation.js
@@ -3,8 +3,21 @@ import { createStackNavigator } from "@react-navigation/stack";
 import Home from "../pages/Home";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ListPets from "../pages/ListPets";
+import Message from "../pages/Message";
 
 const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+function TabRoutes() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name='ListaPets' component={ListPets} />
+      <Tab.Screen name='Mensagens' component={Message} />
+    </Tab.Navigator>
+  )
+}
 
 export default function Navigation() {
   return (
